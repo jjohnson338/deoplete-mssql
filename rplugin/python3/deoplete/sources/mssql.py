76ed56ad361010b6512edce1f0f9725d0785148c
Jared Johnson <jaredj@gmx.com>
2019-08-27 18:49:16 -0400

    Code cleanup, formatting

--- rplugin/python3/deoplete/sources/mssql.py
+++ rplugin/python3/deoplete/sources/mssql.py
@@ -5,18 +5,19 @@ from .base import Base
 from deoplete.util import getlines, parse_buffer_pattern
 from subprocess import CalledProcessError
 
+
 class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
         self.dup = True
-        self.filetypes = [ 'sql' ]
+        self.filetypes = ['sql']
         self.input_pattern = '((\w+)[.]\w*)|([@]\w+)|(\w+)'
         self.is_volatile = True
         self.mark = '[mssql]'
         self.min_pattern_length = 1
         self.name = 'mssql'
         self.rank = 350
-        self.sorters = [ 'sorter_rank', 'sorter_word' ]
+        self.sorters = ['sorter_rank', 'sorter_word']
 
     def on_init(self, context):
         self.server = context['vars'].get('deoplete#sources#mssql#server')
@@ -24,34 +25,34 @@ class Source(Base):
         self.password = context['vars'].get('deoplete#sources#mssql#password')
         self.db = context['vars'].get('deoplete#sources#mssql#db')
         self.command = [
-            'sqlcmd'
-            ,'-S', self.server
-            ,'-U', self.user
-            ,'-P', self.password
-            ,'-d', self.db
-            ,'-h-1'
-            ,'-W'
-            ,'-s', ','
-            ,'-Q', QUERY_STRING
+            'sqlcmd',
+            '-S', self.server,
+            '-U', self.user,
+            '-P', self.password,
+            '-d', self.db,
+            '-h-1',
+            '-W',
+            '-s', ',',
+            '-Q', QUERY_STRING,
         ]
         self._cache = {
-            'tables': {}
-            ,'variables': {}
+            'tables': {},
+            'variables': {}
         }
 
     def gather_candidates(self, context):
         self._make_cache(context)
 
-        current = context['complete_str'] # current search text
+        current = context['complete_str']  # current search text
         candidates = []
 
         if current.startswith("@"):
             # variables
             for variable in self._cache['variables']:
                 candidates.append({
-                    'word': f"@{variable}"
-                    ,'menu': f'[{self._cache["variables"][variable]["type"]}]'
-                    ,'kind': 'variable'
+                    'word': f"@{variable}",
+                    'menu': f'[{self._cache["variables"][variable]["type"]}]',
+                    'kind': 'variable',
                 })
             return candidates
 
@@ -62,34 +63,39 @@ class Source(Base):
             table_or_alias = column_match.group(1)
             for table in self._cache['tables']:
                 is_table = table == table_or_alias.upper()
-                is_alias = table_or_alias.upper() in self._cache['tables'][table]['aliases']
+                is_alias = table_or_alias.upper() \
+                    in self._cache['tables'][table]['aliases']
                 if is_table or is_alias:
                     # append columns
                     for column in self._cache['tables'][table]['columns']:
-                        type_string = (f'{column["type"]}({column["length"]}) {"" if column["nullabe"] else "NOT "}NULL'
+                        type_string = (
+                            f'{column["type"]}({column["length"]})'
+                            f'{"" if column["nullabe"] else "NOT "}NULL'
                             if column["length"] is not None
-                            else f'{column["type"]} {"" if column["nullabe"] else "NOT "}NULL'
-                        )
+                            else (
+                                f'{column["type"]}'
+                                f'{"" if column["nullabe"] else "NOT "}NULL')
+                            )
                         candidates.append({
-                            'word': f"{table_or_alias}.{column['column_name']}"
-                            ,'menu': f'[{type_string}]'
-                            ,'kind': 'col'
+                            'word': f"{table_or_alias}."
+                                    f"{column['column_name']}",
+                            'menu': f'[{type_string}]',
+                            'kind': 'col'
                         })
             candidates.sort(key=operator.itemgetter('word'))
             return candidates
 
-
         # otherwise, fill candidates with all tables and aliases
         for table in self._cache['tables']:
             candidates.append({
-                'word': table
-                ,'kind': f"[{self._cache['tables'][table]['type']}]"
+                'word': table,
+                'kind': f"[{self._cache['tables'][table]['type']}]",
             })
 
             for alias in self._cache['tables'][table]['aliases']:
                 candidates.append({
-                    'word': alias
-                    ,'kind': 'alias'
+                    'word': alias,
+                    'kind': 'alias',
                 })
 
         candidates.sort(key=operator.itemgetter('word'))
@@ -102,21 +108,23 @@ class Source(Base):
         return return_val
 
     def get_complete_position(self, context):
-        variable = self.get_last_match(VARIBLE_PATTERN.finditer(context['input']))
-        column = self.get_last_match(COLUMN_PATTERN.finditer(context['input']))
+        variable = self.get_last_match(
+                VARIBLE_PATTERN.finditer(context['input']))
+        column = self.get_last_match(
+                COLUMN_PATTERN.finditer(context['input']))
         other = re.search(self.input_pattern+"$", context['input'])
         return max(
-            variable.start() if variable is not None else -1
-            ,column.start() if column is not None else -1
-            ,other.start() if other is not None else -1
+            variable.start() if variable is not None else -1,
+            column.start() if column is not None else -1,
+            other.start() if other is not None else -1
         )
 
     def _make_cache(self, context):
         # gather variables
         self._cache['variables'] = {}
         variable_hits = parse_buffer_pattern(
-            getlines(self.vim, 1)
-            ,r'(@)(\w+)(\s+)(\w+)'
+            getlines(self.vim, 1),
+            r'(@)(\w+)(\s+)(\w+)'
         )
         for variable_hit in variable_hits:
             variable = variable_hit[1]
@@ -129,11 +137,12 @@ class Source(Base):
         # populate tables and columns
         if not self._cache['tables']:
             try:
-                command_results = (subprocess
-                    .check_output(self.command, universal_newlines=True)
-                    .split('\n')
+                command_results = (
+                        subprocess
+                        .check_output(self.command, universal_newlines=True)
+                        .split('\n')
                 )
-            except CalledProcessError as e:
+            except CalledProcessError:
                 return None
 
             for row in command_results:
@@ -145,21 +154,24 @@ class Source(Base):
                 type_name = match.group(2).strip()
                 column = match.group(3).strip()
                 column_type = match.group(4).strip()
-                column_nullable = True if match.group(5).strip() == '1' else False
+                column_nullable = (
+                        True if match.group(5).strip() == '1' else False)
                 column_length = match.group(6).strip()
                 column_def = {
-                    'column_name': column
-                    ,'type': column_type
-                    ,'nullabe': column_nullable
-                    ,'length': column_length if column_length != 'NULL' else None
+                    'column_name': column,
+                    'type': column_type,
+                    'nullabe': column_nullable,
+                    'length': (column_length
+                               if column_length != 'NULL'
+                               else None),
                 }
                 if table_or_view not in self._cache['tables']:
                     self._cache['tables'][table_or_view] = {
-                        'type': type_name
-                        ,'columns': [ column_def ]
-                        ,'aliases': []
+                        'type': type_name,
+                        'columns': [column_def],
+                        'aliases': [],
                     }
-                elif not column in self._cache['tables'][table_or_view]:
+                elif column not in self._cache['tables'][table_or_view]:
                     self._cache['tables'][table_or_view]['columns'].append(column_def)
 
         # gather aliases
